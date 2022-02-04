a00228a3156c24e95014d936f13e594830617d10
AnikethDandu <aniketh.dandu@icloud.com>
2022-02-04 00:24:02 -0500

    Update mesh render to create cube

--- HelloMars/Assets/_Scripts/CreateCube.cs
+++ HelloMars/Assets/_Scripts/CreateCube.cs
@@ -4,35 +4,56 @@ using UnityEngine;
 
 public class CreateCube : MonoBehaviour
 {
-
     private Vector3[] vertices = new Vector3[8];
     private Vector2[] uvs;
-    private int[] triangles = new int[6];
+    private int[] triangles = new int[36];
 
     // Start is called before the first frame update
     void Start()
     {
-        Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        LabelVertices();
+        CreateTriangles();
+        CreateMesh();
+    }
 
-        for (int i = 0; i < 8; i++) {
-            vertices[i] = new Vector3(i >> 2, i >> 1 & 1, i & 1);
+    private void LabelVertices() {
+        // Label a cube vertex coordinate system from 0 - 7 (x,y,z)
+        for (int i = 0; i < 8; i++) 
+        {
+            for (int j = 2; j >= 0; j--) {
+                vertices[i][2-j] = (i >> j) & 1;
+            }
         }
+    }
 
+    private void CreateTriangles() {
+        // Create groups of 3 vertices per triangle, splitting each face of a cube into 2 triangles
         triangles = new int[]
         {
-            0,2,1,
-            2,3,1
+            0,1,3,
+            0,3,2,
+            3,6,2,
+            3,7,6,
+            1,7,3,
+            1,5,7,
+            0,6,4,
+            0,2,6,
+            7,4,6,
+            7,5,4,
+            0,4,5,
+            0,5,1
         };
+    }
 
+    private void CreateMesh() {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        // Assign vertices and triangles to mesh filter
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        mesh.Optimize();
+        // Use Unity built-in normal calculations
+        mesh.RecalculateNormals();
     }
 }
