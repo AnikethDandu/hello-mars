773effe801320073f5ab7dbd6a01f3b03668e06b
AnikethDandu <aniketh.dandu@icloud.com>
2022-02-06 01:06:45 -0500

    Add density function and case calculation to Voxel class

--- HelloMars/Assets/_Scripts/CreateMesh.cs
+++ HelloMars/Assets/_Scripts/CreateMesh.cs
@@ -6,10 +6,18 @@ public class CreateMesh : MonoBehaviour
 {
 
     private const int m_voxelCount = 8;
-    private const double m_increment = 1.0/m_voxelCount;
+    private const float m_increment = 1.0f/m_voxelCount;
 
     void Start()
     {
         Voxel v = new Voxel(Vector3.zero, m_increment);
+        v.DensityFunc = DensityFunction;
+        v.CalculateByte();
+        Debug.Log(v.Case);
+    }
+
+    private double DensityFunction(Vector3 pos)
+    {
+        return (Mathf.Sin(pos.x)) * Mathf.Sin(pos.y) - pos.z;
     }
 }
--- HelloMars/Assets/_Scripts/Voxel.cs
+++ HelloMars/Assets/_Scripts/Voxel.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Voxel : MonoBehaviour
+public class Voxel
 {
     // Class variables
-    delegate void DensityFunction();
+    public delegate double DensityFunction(Vector3 pos);
     private static DensityFunction m_func;
-    static private double m_increment;
+    static private float m_increment;
     private Vector3 m_botLeftCoord;
     private byte m_case;
 
@@ -21,24 +22,43 @@ public class Voxel : MonoBehaviour
     public byte Case
     {
         get { return m_case; }
+        set { m_case = value; }
     }
 
-    DensityFunction Func
+    public DensityFunction DensityFunc
     {
         get { return m_func; }
         set { m_func = value;}
     }
 
     // Constructor
-    public Voxel(Vector3 pos, double increment)
+    public Voxel(Vector3 pos, float increment)
     {
         BotLeftCoord = pos;
         m_increment = increment;
+        Case = 0;
     }
 
-    private void CalculateByte()
+    public void CalculateByte()
     {
         Vector3[] coords = new Vector3[8];
         coords[0] = BotLeftCoord;
+        coords[1] = coords[0] + m_increment * Vector3.up;
+        coords[2] = coords[1] + m_increment * Vector3.right;
+        coords[3] = coords[2] + m_increment * Vector3.down;
+        coords[4] = coords[0] + m_increment * Vector3.forward;
+        coords[5] = coords[1] + m_increment * Vector3.forward;
+        coords[6] = coords[2] + m_increment * Vector3.forward;
+        coords[7] = coords[3] + m_increment * Vector3.forward;
+
+        for (int i = 0; i < 8; i++) 
+        {
+            // ret &= (byte)(Convert.ToInt32((m_func(coords[i]) > 0)) << i)
+            // ret |= (m_func(coords[i]) > 0) ? (byte)(1 << i) : (byte)0;
+            if (DensityFunc(coords[i]) > 0)
+            {
+                Case |= (byte)(1 << i);
+            }
+        }
     }
 }
