15493e3a82b0364b4a0a00905592dbb2a1ac98f6
AnikethDandu <aniketh.dandu@icloud.com>
2022-02-06 02:21:03 -0500

    Add lookup tables to Voxel class

--- HelloMars/Assets/_Scripts/CreateMesh.cs
+++ HelloMars/Assets/_Scripts/CreateMesh.cs
@@ -8,6 +8,7 @@ public class CreateMesh : MonoBehaviour
     private const int m_voxelCount = 8;
     private const float m_increment = 1.0f/m_voxelCount;
 
+
     void Start()
     {
         Voxel v = new Voxel(Vector3.zero, m_increment);
@@ -18,6 +19,6 @@ public class CreateMesh : MonoBehaviour
 
     private double DensityFunction(Vector3 pos)
     {
-        return (Mathf.Sin(pos.x)) * Mathf.Sin(pos.y) - pos.z;
+        return (Mathf.Sin(pos.x)) * Mathf.Sin(pos.z) - pos.y;
     }
 }
