2263e73cb7a9ce201e0df76137cebfd254862e2a
AnikethDandu <aniketh.dandu@icloud.com>
2022-02-17 11:44:48 -0500

    Update mesh to create nxnxn voxels and update density function

--- HelloMars/Assets/_Scripts/CreateMesh.cs
+++ HelloMars/Assets/_Scripts/CreateMesh.cs
@@ -8,17 +8,30 @@ public class CreateMesh : MonoBehaviour
     private const int m_voxelCount = 8;
     private const float m_increment = 1.0f/m_voxelCount;
 
-    private Vector3[] vertices;
+    private List<Vector3> vertices = new List<Vector3>();
     // private Vector2[] uvs;
     private int[] triangles;
 
     void Start()
     {
-        Voxel v = new Voxel(Vector3.zero, m_increment, DensityFunction);
-        v.CalculateByte();
-        vertices = v.GetTriangles();
-        triangles = new int[vertices.Length];
-        for (int i = 0; i < vertices.Length; i++)
+        for (int x = 0; x < m_voxelCount; x++)
+        {
+            for (int y = 0; y < m_voxelCount; y++)
+            {
+                for (int z = 0; z < m_voxelCount; z++)
+                {
+                    Voxel v = new Voxel(new Vector3(x, y, z) * (float)(m_increment), m_increment, DensityFunction);
+                    v.CalculateByte();
+                    Vector3[] t = v.GetTriangles();
+                    foreach(Vector3 tri in t)
+                    {
+                        vertices.Add(tri);
+                    }
+                }            
+            }
+        }
+        triangles = new int[vertices.Count];
+        for (int i = 0; i < vertices.Count; i++)
         {
             triangles[i] = i;
         }
@@ -27,7 +40,7 @@ public class CreateMesh : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh;
         // Assign vertices and triangles to mesh filter
         mesh.Clear();
-        mesh.vertices = vertices;
+        mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles;
         mesh.Optimize();
         // Use Unity built-in normal calculations
@@ -37,6 +50,7 @@ public class CreateMesh : MonoBehaviour
 
     private double DensityFunction(Vector3 pos)
     {
-        return (Mathf.Sin(pos.x)) * Mathf.Sin(pos.z) - pos.y - 0.01;
+        return -Mathf.Pow(pos.x - 1/2, 2) - Mathf.Pow(pos.y - 1/2, 2) - Mathf.Pow(pos.z - 1/2, 2) + 1.0/9.0;
+        // return (Mathf.Sin(pos.x)) * Mathf.Sin(pos.z) - pos.y - 0.01;
     }
 }
